7d54db1860c8b54debaace48be5be8566eb1fbb7
пк <@DESKTOP-81SKH73>
2024-10-30 22:53:38 +0200

    Добавьте файлы проекта.

---
+++ lab3.1/Program.cs
@@ -0,0 +1,22 @@
+﻿try
+{
+    Console.WriteLine("Введите n");
+    int n = int.Parse(Console.ReadLine());
+    double sum = 0;
+    for (int k = 1; k <= n; k++)
+    {
+        if (k == 0) continue;
+        int P = 1;
+        for (int j = 1; j <= k; j++) P *= j;
+        double result = (Math.Pow(k - 4, 3) * (k + 7))/P;
+        if (result != 0 && result != double.PositiveInfinity && result != double.NegativeInfinity)
+        {
+            sum += result;
+        }
+    }
+    Console.WriteLine($"Сумма ряда: {sum}");
+}
+catch
+{
+    Console.WriteLine("Введите корректные данные");
+}
